eba3749e97f3f8a154d59bb801164124eac1dfff
Will Bowers <willb0102@gmail.com>
2025-08-06 23:51:24 -0600

    Make edges stick around; use proper visitor pattern for nodes

--- jsongraph.cpp
+++ jsongraph.cpp
@@ -7,9 +7,10 @@
 namespace jsongraph {
 
 struct graph_edge {
-    std::string output_name;
-    std::string name;
-    std::string label;
+    // Don't get confused here, each edge is directed from (node) output -> (node) input
+    std::string output_name, output_label;
+    std::string input_name, input_label;
+    ImVec2 out, in; // These are updated on frame and edges should be rendered after nodes.
 };
 
 struct graph_node {
@@ -23,9 +24,29 @@ struct graph_node {
         bool,
         int, // not sure what type to use for numeric json here
         double, // again, same problem
-        std::vector<graph_node*>, // We are an object (or array) - but that is treated as an object with numeric key strings!
-        graph_edge
+        std::vector<graph_node> // We are an object (or array) - but that is treated as an object with numeric key strings!
     > value;
+};
+
+template<class... Ts>
+struct overloads : Ts... { using Ts::operator()...; };
+
+void visit_trampoline(graph_node& node);
+
+const auto visitor = overloads {
+    [](const std::string& val){ ImGui::Text(val.c_str()); },
+    [](bool & val){ ImGui::Checkbox("##", &val); },
+    [](int & val){ ImGui::InputInt("##", &val); },
+    [](double & val){ ImGui::InputDouble("##", &val); },
+    [](std::vector<graph_node>& val){
+        for (graph_node& node : val)
+            visit_trampoline(node);
+    }
+};
+
+// There must be a better way to do this.
+void visit_trampoline(graph_node& node) {
+    std::visit(visitor, node.value);
 };
 
 std::vector<graph_node> node_list {
@@ -51,74 +72,102 @@ std::vector<graph_node> node_list {
         "D",
         {},
         {1.f, 1.f, 1.f, 1.f},
-        3.14
-    },
-    {
-        "E",
-        {},
-        {1.f, 1.f, 1.f, 1.f},
-        graph_edge{"My Connection", "", ""}
+        std::vector<graph_node> {
+            {
+                "C",
+                {},
+                {1.f, 1.f, 1.f, 1.f},
+                3.14
+            },
+            {
+                "C",
+                {},
+                {1.f, 1.f, 1.f, 1.f},
+                -1.0/12.0
+            }
+        }
     }
 };
 std::vector<graph_edge> edge_list;
 
-std::string new_edge_origin;
+bool is_dragging = false;
+std::string new_edge_origin_window;
+ImVec2 edge_origin_window_pos;
 graph_edge new_edge;
 
-int render_node(GLFWwindow* window, editor_state& state, graph_node& node)
+// Interior check for dragging and dropping. We don't want to do a sphere check
+// here - we want it to be fast and err on the side of allowing the drop to happen.
+inline auto points_overlap = [](ImVec2 a, ImVec2 b, float buffer) -> bool {
+    if (a.x - b.x > buffer || b.x - a.x > buffer) return false;
+    if (a.y - b.y > buffer || b.y - a.y > buffer) return false;
+    return true;
+};
+
+void render_edge(const graph_edge& e) {
+    float dist = sqrt((e.in.x - e.out.x)*(e.in.x - e.out.x)
+                    + (e.in.y - e.out.y)*(e.in.y - e.out.y));
+    auto outvec = ImVec2(e.out.x + (dist*0.5), e.out.y);
+    auto invec = ImVec2(e.in.x - (dist*0.5), e.in.y);
+    ImGui::GetForegroundDrawList()->AddBezierCubic(e.out, outvec, invec, e.in, IM_COL32_WHITE, 2.5f);
+}
+
+void render_node(GLFWwindow* window, editor_state& state, graph_node& node)
 {
     ImGuiIO& io = ImGui::GetIO(); (void)io; // TODO Does this involve a virtual function call? If not, leave as is.
 
+    if (node.key == new_edge_origin_window)
+        ImGui::SetNextWindowPos(edge_origin_window_pos);
+
     const auto flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize;
     ImGui::Begin(node.key.c_str(), nullptr, flags);
-    // TODO visitor
-    switch (node.value.index()) {
-        case 0:
-            ImGui::Text(std::get<std::string>(node.value).c_str());
-            break;
-        case 1:
-            ImGui::Checkbox("##", &std::get<bool>(node.value));
-            break;
-        case 2:
-            ImGui::InputInt("##", &std::get<int>(node.value));
-            break;
-        case 3:
-            ImGui::InputDouble("##", &std::get<double>(node.value));
-            break;
-        case 4:
-            break;
-        case 5:
-            auto& edge = std::get<graph_edge>(node.value);
-            ImGui::Text(edge.output_name.c_str());
-            break;
-    }
+    std::visit(visitor, node.value);
+
     ImDrawList* draw_list = ImGui::GetForegroundDrawList();
     const ImVec2 cur_pos = ImGui::GetCursorScreenPos();
     const ImVec2 win_pos = ImGui::GetWindowPos();
     const ImVec2 win_size = ImGui::GetWindowSize();
     
     // why does 3/5 look better than 1/2?
-    auto start = ImVec2(win_pos.x + win_size.x, cur_pos.y - 3*ImGui::GetTextLineHeight()/5);
+    float ylevel = cur_pos.y - 3*ImGui::GetTextLineHeight()/5;
+    if (ImGui::IsWindowCollapsed())
+        ylevel = win_pos.y + (win_size.y / 2);
+
+    auto in = ImVec2(win_pos.x, ylevel);
+    auto out = ImVec2(win_pos.x + win_size.x, ylevel);
 
     float r = 5.0f;
-    if (!ImGui::IsWindowCollapsed()) {
-        draw_list->AddCircle(start, r, node.color, 16, 2.0f);
-        if (io.MouseDown[0]
-        && io.MouseClickedPos[0].x < (start.x + r) // todo this is stupid
-        && io.MouseClickedPos[0].x > (start.x - r)
-        && io.MouseClickedPos[0].y < (start.y + r)
-        && io.MouseClickedPos[0].y > (start.y - r)
-        ) {
-            float dist = sqrt((io.MousePos.x - start.x)*(io.MousePos.x - start.x)
-                        + (io.MousePos.y - start.y)*(io.MousePos.y - start.y));
-            auto outvec = ImVec2(start.x + (dist*0.5), start.y);
-            auto invec = ImVec2(io.MousePos.x - (dist*0.5), io.MousePos.y);
-            ImGui::GetForegroundDrawList()->AddBezierCubic(start, outvec, invec, io.MousePos, node.color, 2.5f);
-        }
-    }
-    ImGui::End();
+    draw_list->AddCircle(out, r, node.color, 16, 2.0f);
+    draw_list->AddCircle(in, r, node.color, 16, 2.0f);
 
-    return 0;
+    for (graph_edge& edge : edge_list) {
+        if (edge.output_name == node.key)
+            edge.out = out;
+        if (edge.input_name == node.key)
+            edge.in = in;
+    }
+
+    if (io.MouseDown[0] && points_overlap(io.MouseClickedPos[0], out, r)) {
+        is_dragging = true;
+
+        // Lock the window
+        new_edge_origin_window = node.key;
+        edge_origin_window_pos = ImGui::GetWindowPos();
+        
+        new_edge.output_name = node.key;
+        new_edge.out = out;
+        new_edge.in = io.MousePos;
+    } else if (is_dragging && !io.MouseDown[0]
+            && node.key != new_edge.output_name // No. Just no.
+            && points_overlap(io.MousePos, in, r)) {
+        is_dragging = false;
+        new_edge_origin_window = ""; // clear window lock
+        new_edge.input_name = node.key;
+        new_edge.in = in;
+        edge_list.push_back(new_edge);
+        new_edge = graph_edge{};
+    }
+
+    ImGui::End();
 }
 
 int render(GLFWwindow* window, editor_state& state)
@@ -156,6 +205,14 @@ int render(GLFWwindow* window, editor_state& state)
 
     for (graph_node& node : node_list)
         render_node(window, state, node);
+
+    for (const graph_edge& edge : edge_list)
+        render_edge(edge);
+    
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    is_dragging = is_dragging && io.MouseDown[0];
+    if (is_dragging)
+        render_edge(new_edge);
 
     // Rendering
     ImGui::Render();
